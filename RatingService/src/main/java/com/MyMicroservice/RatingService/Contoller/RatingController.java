9e16a5ca5072728457659005c46529793e01a785
ishwar chaiser <ishwarchaiser@gmail.com>
2023-11-19 12:23:15 +0530

    Dynamic rest call using service discovery and resttemplate

--- RatingService/src/main/java/com/MyMicroservice/RatingService/Contoller/RatingController.java
+++ RatingService/src/main/java/com/MyMicroservice/RatingService/Contoller/RatingController.java
@@ -1,14 +1,21 @@
 package com.MyMicroservice.RatingService.Contoller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/rating")
 public class RatingController {
+    public static final String EMPLOYEE_URL="http://EMPLOYEE-APP/employee/get";
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/get")
     public String get(){
-        return "Rating Controller Hit!!!";
+        String employeeDetailResponse = restTemplate.getForObject(EMPLOYEE_URL,String.class);
+        return "Rating Controller Hit!!! and employeeDetailResponse is : "+employeeDetailResponse;
     }
 }
